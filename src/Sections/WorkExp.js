305542525ab15f6faba0a078acbf41b99a22386f
kip0215 <67665667+kippumchagn215@users.noreply.github.com>
2024-07-04 17:36:43 -0500

    Update WorkExp.js

--- src/Sections/WorkExp.js
+++ src/Sections/WorkExp.js
@@ -1,16 +1,56 @@
 import React from "react";
 
-export default function WorkExp(){
-    return(
-        <div id="workexp" class="bg-gray padding-bottom white-text">
-        <h1 class="Header">Work Experience</h1>
-        <h3 class="center">Software development intern at Tyler Technologies - (2021 May - 2021 August, 3 months)</h3>
+export default function WorkExp() {
+  return (
+    <div id="workexp" class="bg-gray padding-bottom white-text">
+      <h1 class="Header">Work Experience</h1>
+      <h3 class="left">
+        Software Developer at ClothingTech LLC - (July 2022 - Present)
         <br></br>
-        <p class="center"><b>Research.TX(app to share court cases)</b> - used <u>Angular.js,.Netcore</u> with <u>C#</u> and <u>MSsql</u> worked on improving a tool for admin </p>
-<p class="center">to solve the integration error between the company's application and case management system of each court.</p> 
-<p class="center"><b>Netsparker</b> - set up an environment to scan for vulnerabilities within a web application using Netsparker.</p> 
-<p class="center"><b>Webload</b>  - tracked all API calls made from the website using webload, and created a script for load testing.</p>
-
-        </div>
-    );
+        <h5>- Player of Sprint 77 and 80</h5>
+      </h3>
+      <br></br>
+      <p class="left">
+        Implemented API calls to ensure seamless connectivity between the{" "}
+        <u>MS SQL </u>
+        database system, the backend with simulation logic written on{" "}
+        <u>.NET Core </u>
+        with <u>C#</u>, and the user interface, enabling efficient interactions
+        between the different components of the system.
+      </p>
+      <p class="left">
+        Enhanced the overall user experience by leveraging the features of the
+        <u> Unity</u> platform and developed new features to enable users to
+        edit the shape of garments, providing more customization options.
+      </p>
+      <p class="left">
+        Significantly improved the realism of garment visualizations through the
+        integration of simulatable trims, adding thickness based on the fabric
+        type to enhance the sense of reality, and implementing meshes to add
+        texture to the garments. These enhancements were achieved by employing
+        mathematical concepts and leveraging the cloth simulation library,
+        <u>Syflex</u>, in <u>C++</u>.
+      </p>
+      <br></br>
+      <h3 class="left">
+        Software Development Intern at Tyler Technologies - (May 2021 - August
+        2021, 3 months)
+      </h3>
+      <br></br>
+      <p class="left">
+        Worked With <u>Angular.js</u>, <u>.Net Core</u> with <u>C#</u> and{" "}
+        <u>MS SQL</u>. Worked on improving a tool for admin to solve the
+        integration error between the company's application and case management
+        system of each court.
+      </p>
+      <p class="left">
+        <u>Netsparker</u> - set up an environment to scan for vulnerabilities
+        within a web application using Netsparker.
+      </p>
+      <p class="left">
+        Tracked all API calls made from the website using <u>Webload</u>, and
+        created a script for load testing.
+      </p>
+    </div>
+  );
 }
