fe16fc175a07a2db0dbcb528121a3cb3adac651b
kip0215 <67665667+kippumchagn215@users.noreply.github.com>
2024-07-04 17:36:03 -0500

    Update ContactMe.js

--- src/Sections/ContactMe.js
+++ src/Sections/ContactMe.js
@@ -1,17 +1,17 @@
-import React from "react"
-import {Container} from "react-bootstrap"
+import React from "react";
+import { Container } from "react-bootstrap";
 import ContactCard from "../Components/ContactMe_Component/ContactCard";
 import ContactForm from "../Components/ContactMe_Component/ContactForm";
-export default function ContactMe(){
-    function preventDefault(e){
-        e.preventDefault();
-    }
-    return(
-        <div class="">
-            <h1 class="Header" id="contact">Contact Me</h1>
-            <ContactForm></ContactForm>
-            <ContactCard></ContactCard>
-        </div>
-        
-    );
+export default function ContactMe() {
+  function preventDefault(e) {
+    e.preventDefault();
+  }
+  return (
+    <div class="">
+      <br></br>
+      <br></br>
+      <br></br>
+      <ContactCard></ContactCard>
+    </div>
+  );
 }
