35d006e57a620cf2deeca7ab65da8cd952a3bec0
kip0215 <67665667+kippumchagn215@users.noreply.github.com>
2024-07-04 17:35:30 -0500

    Update AboutMe.js

--- src/Sections/AboutMe.js
+++ src/Sections/AboutMe.js
@@ -1,23 +1,50 @@
-import React from "react"
-import {Container,Row,Col,Button} from "react-bootstrap"
-function AboutMe(){
-    return(
-        <div id="aboutme">
-            <Container>
-                <h1 class="Header">About Me</h1>
-                <Row>
-                    <Col lg={4} md={12}><div class="Selfie"><img class="MyPhoto" src = "/images/profile_img/Selfie.jpg" alt = "selfie" height="300px" width="300px"></img></div></Col>
-                    <Col lg={8} md={12}><h5>Hey, I'm Kippum, a software developer and currently a student pursuing a bachelor's degree at the University of Texas at Dallas who is interested in full-stack web application development.
-                        <br></br><br></br>My goal is to develop an application that can provide convenience to people and to achieve this goal I have been obtaining the necessary skills and knowledge through school, personal projects and, an internship. 
-                        <br></br><br></br>Also, in my free time, I enjoy playing sports especially table tennis, making and playing games, and watching TV shows</h5>
-                        <br></br>
-                        <Button variant="dark" href="https://docs.google.com/document/d/1luOxDYdrQ7ENP1RqTUwVBiJg12d_asX3UnQss-gOrgM/edit?usp=sharing">Resume</Button>
-                        </Col>
-                        
-                </Row>
-            </Container>
-        </div>
-    );
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+function AboutMe() {
+  return (
+    <div id="aboutme">
+      <Container>
+        <h1 class="Header">About Me</h1>
+        <Row>
+          <Col lg={4} md={12}>
+            <div class="Selfie">
+              <img
+                class="MyPhoto"
+                src="/images/profile_img/Selfie.jpg"
+                alt="selfie"
+                height="300px"
+                width="300px"
+              ></img>
+            </div>
+          </Col>
+          <Col lg={8} md={12}>
+            <h5>
+              Hi, I'm Kippum, experienced software developer with a diverse
+              background spanning Unity game development, mathematical coding in
+              <b> C# .NET,</b> and simulation improvement in <b>C++. </b>
+              Skilled in writing and maintaining robust mathematical algorithms
+              and simulation codes. <br></br>
+              <br></br>Proficient in web technologies including
+              <b> React.js, Angular.js, and Node.js, </b>
+              with hands-on experience in database management systems such as{" "}
+              <b>Apache CouchDB, SQL, and MongoDB.</b>
+              <br></br>
+              <br></br>Also, in my free time, I enjoy playing sports especially
+              <b> pickleball!!!!</b>, table tennis, making and playing games,
+              and watching TV shows
+            </h5>
+            <br></br>
+            <Button
+              variant="dark"
+              href="https://docs.google.com/document/d/1luOxDYdrQ7ENP1RqTUwVBiJg12d_asX3UnQss-gOrgM/edit?usp=sharing"
+            >
+              Resume
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default AboutMe;
